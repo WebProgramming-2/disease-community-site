3ba07aeda88757d7a9f5b1fffbdad4831f54beb2
soyeon <soyeon20031010@gmail.com>
2023-11-28 18:14:32 +0900

    chore: 빌드

---
+++ MyPageService/src/main/java/com/example/mypageservice/MyPageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mypageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPageServiceApplication.class, args);
+	}
+
+}
