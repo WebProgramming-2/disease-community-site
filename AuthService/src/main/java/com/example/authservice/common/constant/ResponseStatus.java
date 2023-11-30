adad929bae6b905fabc0922073ad3ba9eee04c7a
koseonje <kosunje1455@navet.com>
2023-12-01 01:47:47 +0900

    feat: ResponseMessage 구현

---
+++ AuthService/src/main/java/com/example/authservice/common/constant/ResponseStatus.java
@@ -0,0 +1,19 @@
+package com.example.authservice.common.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ResponseStatus {
+  OK("요청에 성공하였습니다."),
+  BAD_REQUEST("잘못된 요청입니다."),
+  SEND_MAIL_FAIL("인증 번호 전송에 실패하였습니다."),
+  BAD_CREDENTIAL("아이디 혹은 비밀번호가 일치하지 않습니다."),
+  UNAUTHORIZED("로그인 후 접근 가능합니다."),
+  FORBIDDEN("접근 권한이 없습니다."),
+  INTERNAL_SEVER_ERROR("서버 에러"),
+  DUPLICATED_EMAIL("이미 사용 중인 이메일입니다.");
+
+  private final String resultMessage;
+}
---
+++ AuthService/src/main/java/com/example/authservice/common/message/ResponseMessage.java
@@ -0,0 +1,31 @@
+package com.example.authservice.common.message;
+
+import com.example.authservice.common.constant.ResponseStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ResponseMessage {
+
+  private ResponseStatus responseStatus;
+  private String resultMessage;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private Object data;
+
+  public static ResponseMessage of(ResponseStatus responseStatus, Object data) {
+    return ResponseMessage.builder()
+        .responseStatus(responseStatus)
+        .resultMessage(responseStatus.getResultMessage())
+        .data(data)
+        .build();
+  }
+
+  public static ResponseMessage of(ResponseStatus responseStatus) {
+    return ResponseMessage.builder()
+        .responseStatus(responseStatus)
+        .resultMessage(responseStatus.getResultMessage())
+        .build();
+  }
+}
