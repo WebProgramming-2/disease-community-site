57bcdbc457101ca35079906e8accf5991f2acaa4
koseonje <kosunje1455@navet.com>
2023-12-18 23:25:09 +0900

    feat: 기능추가

--- MainPageService/src/main/java/com/example/mainpageservice/controller/MainPageController.java
+++ MainPageService/src/main/java/com/example/mainpageservice/controller/MainPageController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,6 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Controller
+@RequestMapping("/mainPage-service")
 public class MainPageController {
     private final MainPageService mainPageService;
 
