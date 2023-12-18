513a5bcadfa41209d204b7a299612732e587fc9f
user <user>
2023-12-18 19:36:28 +0900

    feat: API 구현

--- MainPageService/src/main/java/com/example/mainpageservice/controller/MainPageController.java
+++ MainPageService/src/main/java/com/example/mainpageservice/controller/MainPageController.java
@@ -6,14 +6,16 @@ import com.example.mainpageservice.dto.response.PostDTO;
 import com.example.mainpageservice.service.MainPageService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-@RestController
+@RequestMapping("/mainPage-service")
+@Controller
 public class MainPageController {
     private final MainPageService mainPageService;
 
@@ -22,17 +24,17 @@ public class MainPageController {
     }
 
     @GetMapping("/popular-posts")
-    public List<PostDTO> getPopularPosts() {
+    public @ResponseBody List<PostDTO> getPopularPosts() {
         return mainPageService.getPopularPosts();
     }
 
     @GetMapping("/category-list")
-    public List<CategoryDTO> getCategoryList() {
+    public @ResponseBody List<CategoryDTO> getCategoryList() {
         return mainPageService.getCategoryList();
     }
 
-    @GetMapping("/posts")
-    public PostsResponseDTO getPosts(
+    @GetMapping("/post")
+    public @ResponseBody PostsResponseDTO getPosts(
             @RequestParam(name = "keyword") String keyword,
             @RequestParam(name = "searchType", defaultValue = "title") String searchType,
             @RequestParam(name = "sortType", defaultValue = "like") String sortType,
@@ -44,5 +46,33 @@ public class MainPageController {
         response.addCookie(mainPageService.updateRecentSearchesCookie(keyword, recentSearches));
         Page<PostDTO> postPage = mainPageService.getPosts(keyword, searchType, sortType, userDisplay, pageNum - 1);
         return new PostsResponseDTO(postPage.getContent(), postPage.getNumber() + 1, postPage.getTotalPages(), postPage.getSize(), postPage.getTotalElements());
+    }
+
+    @GetMapping("/")
+    public String getMainPage(Model model,
+                              @CookieValue(value = "recentSearches", required = false) String recentSearches
+    ) {
+        model.addAttribute("categoryList", this.getCategoryList());
+        model.addAttribute("popularPosts", this.getPopularPosts());
+        if(recentSearches==null) {recentSearches = "";}
+        model.addAttribute("recentSearches", new ArrayList<>(Arrays.asList(recentSearches.split("/"))));
+        return "MainPage";
+    }
+
+    @GetMapping("/posts")
+    public String getSearchPage(Model model,
+                                @RequestParam(name = "keyword") String keyword,
+                                @RequestParam(name = "searchType", defaultValue = "title") String searchType,
+                                @RequestParam(name = "sortType", defaultValue = "like") String sortType,
+                                @RequestParam(name = "userDisplay", defaultValue = "10") int userDisplay,
+                                @RequestParam(name = "pageNum", defaultValue = "1") int pageNum,
+                                HttpServletResponse response,
+                                @CookieValue(value = "recentSearches", required = false) String recentSearches
+    ) {
+        model.addAttribute("categoryList", this.getCategoryList());
+        model.addAttribute("posts", this.getPosts(keyword, searchType, sortType, userDisplay, pageNum, response, recentSearches));
+        if(recentSearches==null) {recentSearches = "";}
+        model.addAttribute("recentSearches", new ArrayList<>(Arrays.asList(recentSearches.split("/"))));
+        return "SearchPage";
     }
 }
